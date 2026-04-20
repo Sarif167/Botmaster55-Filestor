eeb043453f9de7d484f0dc7301ea5299f1639038
Botmaster55 <wajsarif461@gmail.com>
2026-04-20 07:38:03 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,12 @@
+# ================== BOT CONFIG ==================
+
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+# ================== CHANNEL CONFIG ==================
+
+POST_CHANNEL = -100XXXXXXXXXX
+
+FSUB_CHANNEL_1 = -1001111111111
+FSUB_CHANNEL_2 = -1002222222222
